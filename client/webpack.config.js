cb045e711496e9da0d0e59629630b1acfcf0855a
Rahul Raj <rahulraz1308@gmail.com>
2021-07-17 14:00:29 +0530

    initial setup webpack

---
+++ client/webpack.config.js
@@ -0,0 +1,63 @@
+const path = require('path')
+const htmlWebpackPlugin = require('html-webpack-plugin')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+
+module.exports = {
+  mode: process.env.NODE_ENV,
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'build'),
+    filename: 'main.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /node_modules/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env'],
+          },
+        },
+      },
+      {
+        test: /\.scss$/,
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+          'sass-loader',
+        ],
+      },
+      {
+        test: /\.(png|jpe?g|gif|svg|ttf|woff|otf)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              name: '[name].[contenthash].[ext]',
+              outputPath: 'static/img',
+            }
+          }
+        ]
+      }
+    ],
+  },
+  devServer: {
+    hot: true,
+    contentBase: path.resolve(__dirname, 'build'),
+    publicPath: '/',
+  },
+  devtool: 'inline-source-map',
+  plugins: [
+    new htmlWebpackPlugin({
+      template: './public/index.html',
+      filename: 'index.html',
+    }),
+    new MiniCssExtractPlugin({
+      filename: '[name].css',
+    }),
+  ],
+}
